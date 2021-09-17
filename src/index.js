3ead6969d525f3825bc84283e42d38656e952a00
yoon-junseo <ysj6844@naver.com>
2021-09-17 13:19:57 +0900

    Feature: devtool 제거

--- src/index.js
+++ src/index.js
@@ -3,23 +3,19 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
-import SWRDevtools from '@jjordy/swr-devtools';
-import { cache, mutate } from 'swr';
 import rootReducer, { rootSaga } from './modules';
 import App from './App';
 import './index.css';
 
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 
 sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <SWRDevtools cache={cache} mutate={mutate} />
       <App />
     </BrowserRouter>
   </Provider>,
